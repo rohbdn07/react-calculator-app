de58c722a9c28515601448d8fee06c515fb7461a
unknown <rohit.raj.bhandari07@gmail.com>
2020-09-11 01:07:25 +0300

    layout is created

--- src/App.js
+++ src/App.js
@@ -12,15 +12,23 @@ function App() {
       <div className='container'>
         <button id='clear'>Clear</button>
         <button id='c'>C</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>X</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>/</button>
         <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
+        <button>0</button>
+
+        <button id='result'>Total</button>
       </div>
     </div>
   );
